03852c567a81c932b8e042ba452d2e79fd8717ed
Sebastian <19Sebastian95@gmx.de>
2020-12-02 12:10:09 +0100

    Map parse function to rust code

--- libpg_query_rust/src/main.rs
+++ libpg_query_rust/src/main.rs
@@ -6,23 +6,54 @@ use std::ffi::{CString, CStr};
 use std::os::raw::c_char;
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-fn parseQuery(query: &str) -> String {
+pub struct PgErrorInfo {
+    pub message: String,
+    pub funcname: String,
+    pub filename: String,
+    pub lineno: i32,
+    pub cursorpos: i32,
+    pub context: String,
+}
+
+fn c_str_to_str(input: *mut ::std::os::raw::c_char) -> Option<String> {
+    if input.is_null() {
+        return None;
+    }
+    let c_str: &CStr = unsafe { CStr::from_ptr(input) };
+    let str_slice: &str = c_str.to_str().unwrap();
+    let str_buf: String = str_slice.to_owned();
+    return Some(str_buf);
+}
+
+fn parse_query(query: &str) -> Result<String, PgErrorInfo> {
     let c_str = CString::new(query).unwrap();
     let c_ptr: *const c_char = c_str.as_ptr() as *const c_char;
     let parse_tree: *mut ::std::os::raw::c_char;
     unsafe {
-        parse_tree = pg_query_parse(c_ptr).parse_tree;
-    }
+        let output = pg_query_parse(c_ptr);
+        parse_tree = output.parse_tree;
 
-    let c_str: &CStr = unsafe { CStr::from_ptr(parse_tree) };
-    let str_slice: &str = c_str.to_str().unwrap();
-    let str_buf: String = str_slice.to_owned();
-    return str_buf;
+        if output.error.is_null() {
+            return Ok(c_str_to_str(parse_tree).unwrap_or(String::from("[]")));
+        }
+        return Err(PgErrorInfo {
+            message: c_str_to_str((*output.error).message).unwrap_or(String::from("unknown")),
+            funcname: c_str_to_str((*output.error).funcname).unwrap_or(String::from("unknown")),
+            filename: c_str_to_str((*output.error).filename).unwrap_or(String::from("unknown")),
+            lineno: (*output.error).lineno,
+            cursorpos: (*output.error).cursorpos,
+            context: c_str_to_str((*output.error).context).unwrap_or(String::from("unknown")),
+
+        });
+    }
 }
 
 fn main() {
     // Statements here are executed when the compiled binary is called
 
     // Print text to the console
-    println!("{}", parseQuery("SELECT 1"));
+    match parse_query("ELECT 1") {
+        Ok(v) => println!("{}", v),
+        Err(e) => println!("Error: {}", e.message)
+    }
 }
